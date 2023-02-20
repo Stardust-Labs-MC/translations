735dadc0d896404f69f63e7593b00fd8b1f3c11d
catter1 <63272345+catter1@users.noreply.github.com>
2023-02-20 18:05:59 +0100

    Unify language adder script

--- create.py
+++ create.py
@@ -2,17 +2,17 @@ import json
 import os
 
 def main():
-    component = input("Choose component (incendium): ")
     lang = input("Choose language (en_us): ")
+    
+    for component in ["incendium", "omni-biome"]:
+        with open(f"{os.getcwd()}/{component}/en_us.json", 'r') as f:
+            data = json.load(f)
 
-    with open(f"{os.getcwd()}/{component}/en_us.json", 'r') as f:
-        data = json.load(f)
+        for string in data:
+            data[string] = ""
 
-    for string in data:
-        data[string] = ""
-
-    with open(f"{os.getcwd()}/{component}/{lang}.json", 'w') as f:
-        json.dump(data, f, indent=2)
+        with open(f"{os.getcwd()}/{component}/{lang}.json", 'w') as f:
+            json.dump(data, f, indent=2)
 
 if __name__ == '__main__':
     main()
