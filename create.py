0e5ef3c02db2c884f192e96b031340e44d6919bd
catter1 <63272345+catter1@users.noreply.github.com>
2023-09-09 13:30:50 +0200

    Add language list

--- create.py
+++ create.py
@@ -4,6 +4,7 @@ import os
 def main():
     lang = input("Choose language (en_us): ")
     
+    # Copy English strings
     for component in ["incendium", "omni-biome"]:
         with open(f"{os.getcwd()}/{component}/en_us.json", 'r') as f:
             data = json.load(f)
@@ -11,8 +12,18 @@ def main():
         for string in data:
             data[string] = ""
 
+        # Duplicate into new lang
         with open(f"{os.getcwd()}/{component}/{lang}.json", 'w') as f:
             json.dump(data, f, indent=2)
+
+    # Add new lang to lang list
+    with open(f"{os.getcwd()}/languages.json", 'r') as f:
+        langs = json.load(f)
+    
+    langs.append(lang)
+
+    with open(f"{os.getcwd()}/languages.json", 'w') as f:
+        json.dump(langs, f, indent=2)
 
 if __name__ == '__main__':
     main()
